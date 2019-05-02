7da5e97cfdd610126a196126c7d19ff109965529
Ladex Gaming <fullescapeladex@gmail.com>
2019-05-02 16:46:33 +0200

    creation de l'interface basique

--- iot_mobile_app/PimpMyCard/src/app/tab2/tab2.page.ts
+++ iot_mobile_app/PimpMyCard/src/app/tab2/tab2.page.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { threadId } from 'worker_threads';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+export class Tab2Page {
+  temp: number = 0;
+
+
+  constructor() {
+    this.setTemp();
+  }
+
+  setTemp() {
+    this.temp = 42;
+  }
+}
