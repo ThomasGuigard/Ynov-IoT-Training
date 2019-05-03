c7d422ca02c694e6767e8dce10dec799e382f84b
DESKTOP-3G06E02\Antoine <antoinekeribin@hotmail.fr>
2019-05-03 14:46:42 +0200

    temperature displayed on tab 1

--- iot_mobile_app/PimpMyCard/src/app/tab1/tab1.page.ts
+++ iot_mobile_app/PimpMyCard/src/app/tab1/tab1.page.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WebsocketService } from '../IoT/websocket.service';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page {
+  temp: number = 0;
+  websocketService : any = null;
+  initialDelay : number;
+  period : number;
+
+  constructor(websocketService : WebsocketService) {
+    this.websocketService = websocketService;
+  }
+
+  ngOnInit() {
+    this.websocketService.socketTemp.onmessage = async (msg) => {
+      console.log("json : ",JSON.parse(msg.data).temp);
+      this.temp = parseFloat(JSON.parse(msg.data).temp);
+    };
+  }
+}
--- iot_mobile_app/PimpMyCard/src/app/tab2/tab2.page.ts
+++ iot_mobile_app/PimpMyCard/src/app/tab2/tab2.page.ts
@@ -1,20 +1,8 @@
 import { Component } from '@angular/core';
-import { threadId } from 'worker_threads';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
-  temp: number = 0;
-
-
-  constructor() {
-    this.setTemp();
-  }
-
-  setTemp() {
-    this.temp = 42;
-  }
-}
+export class Tab2Page {}
