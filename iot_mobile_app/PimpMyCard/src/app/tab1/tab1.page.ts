3d46ccb16267c6641f90af5baf78c139378c9d01
DESKTOP-3G06E02\Antoine <antoinekeribin@hotmail.fr>
2019-05-10 12:28:44 +0200

    speed displayed + humArray filled + accArray filled

--- iot_mobile_app/PimpMyCard/src/app/tab1/tab1.page.ts
+++ iot_mobile_app/PimpMyCard/src/app/tab1/tab1.page.ts
@@ -11,6 +11,8 @@ import { WebsocketService } from '../IoT/websocket.service';
 export class Tab1Page {
   temp: any = 0;
   pastTempArray = new Array<any>();
+  pastHumArray = new Array<any>();
+  pastAccArray = new Array<any>();
   humi: any = 0;
   acce: any = "[100,200,300]";
   speed: any = 0;
@@ -28,16 +30,17 @@ export class Tab1Page {
 
   ngOnInit() {
     this.websocketService.socketTemp.onmessage = async (msg) => {
-      //console.log("json : ", JSON.parse(msg.data));
       this.addNewTempInArray(JSON.parse(msg.data));
       this.temp = parseFloat(JSON.parse(msg.data).temp);
     };
     this.websocketService.socketHum.onmessage = async (msg) => {
-      //console.log("json : ", JSON.parse(msg.data));
+      this.addNewHumInArray(JSON.parse(msg.data));
       this.humi = JSON.parse(msg.data).hum;
     };
     this.websocketService.socketAcc.onmessage = async (msg) => {
       this.acce = JSON.parse(msg.data).acc;
+      this.addNewAccInArray(JSON.parse(msg.data));
+      this.updateSpeed();
       this.calculateXAngle(this.acce);
     };
   }
@@ -50,16 +53,59 @@ export class Tab1Page {
         this.pastTempArray[index] = newTemp;
       }
     }
-    console.log("array après: ", this.pastTempArray);
+    console.log("array temp après: ", this.pastTempArray);
+  }
+
+  addNewHumInArray(newHum : any){
+    for (let index = 0; index < this.pastHumArray.length; index++) {
+      if (index != 9) {
+        this.pastHumArray[index] = this.pastHumArray[index + 1];
+      } else {
+        this.pastHumArray[index] = newHum;
+      }
+    }
+    console.log("array hum après: ", this.pastHumArray);
+  }
+
+  addNewAccInArray(newAcc : any){
+    for (let index = 0; index < this.pastAccArray.length; index++) {
+      if (index != 9) {
+        this.pastAccArray[index] = this.pastAccArray[index + 1];
+      } else {
+        this.pastAccArray[index] = newAcc;
+      }
+    }
+    console.log("array acc après: ", this.pastAccArray);
   }
 
   ngAfterViewInit() {
-    console.log(this.httpClient);
     this.httpClient.get('http://192.168.43.136:1880/temperature', {}).subscribe((data) => {
       for (let index = 0; index <= 9; index++) {
         this.pastTempArray.push(data[index]);
       }
-      console.log(this.pastTempArray);
+      //console.log(this.pastTempArray);
+    }, (error) => {
+      console.log(error.status);
+      console.log(error.error);// error message as string
+      console.log(error.headers);
+    });
+    this.httpClient.get('http://192.168.43.136:1880/humidity', {}).subscribe((data) => {
+      for (let index = 0; index <= 9; index++) {
+        this.pastHumArray.push(data[index]);
+      }
+      //console.log(this.pastHumArray);
+    }, (error) => {
+      console.log(error.status);
+      console.log(error.error);// error message as string
+      console.log(error.headers);
+    });
+    this.httpClient.get('http://192.168.43.136:1880/acceleration', {}).subscribe((data) => {
+      for (let index = 0; index <= 9; index++) {
+        console.log(data[index]);
+        this.pastAccArray.push(data[index]);
+      }
+      this.calculateSpeedWithArray();
+      //console.log(this.pastAccArray);
     }, (error) => {
       console.log(error.status);
       console.log(error.error);// error message as string
@@ -81,5 +127,63 @@ export class Tab1Page {
       accxyzArray[index] = accxyzArray[index].replace('[', '').replace(']', '');
     }
     return accxyzArray;
+  }
+
+  calculateSpeedWithArray(){
+    var interval = 1;//1 seconde
+    var v0 = 0;
+    var vx0 = 0;
+    var vy0 = 0;
+    var vz0 = 0;
+    var vfx = 0;
+    var vfy = 0;
+    var vfz = 0;
+    var vf = 0;
+    var gravity = 1000;
+    var accXYZ = null;
+    if(this.pastAccArray.length === 10){
+      for (let index = 0; index < this.pastAccArray.length; index++) {
+        accXYZ = this.formatAccData(this.pastAccArray[index].acc);
+        vfx = vx0 + (accXYZ[0]/gravity)*interval;
+        vfy = vy0 + (accXYZ[0]/gravity)*interval;
+        vfz = vz0 + (accXYZ[0]/gravity)*interval;
+        vf = v0 + Math.sqrt(Math.pow(vfx,2) + Math.pow(vfy,2) + Math.pow(vfz,2));
+        vx0 = vfx;
+        vy0 = vfy;
+        vz0 = vfz;
+        v0 = vf;
+      }
+      vf=vf*3,6;//convert in km/h
+      this.speed = vf.toFixed(2);
+    }
+  }
+
+  updateSpeed(){
+    var interval = 1;//1 seconde
+    var v0 = 0;
+    var vx0 = 0;
+    var vy0 = 0;
+    var vz0 = 0;
+    var vfx = 0;
+    var vfy = 0;
+    var vfz = 0;
+    var vf = 0;
+    var gravity = 1000;
+    var accXYZ = null;
+    if(this.pastAccArray.length === 10){
+      for (let index = 8; index < this.pastAccArray.length; index++) {
+        accXYZ = this.formatAccData(this.pastAccArray[index].acc);
+        vfx = vx0 + (accXYZ[0]/gravity)*interval;
+        vfy = vy0 + (accXYZ[0]/gravity)*interval;
+        vfz = vz0 + (accXYZ[0]/gravity)*interval;
+        vf = v0 + Math.sqrt(Math.pow(vfx,2) + Math.pow(vfy,2) + Math.pow(vfz,2));
+        vx0 = vfx;
+        vy0 = vfy;
+        vz0 = vfz;
+        v0 = vf;
+      }
+      vf=vf*3,6;//convert in km/h
+      this.speed = vf.toFixed(2);
+    }
   }
 }
